fbaece5b8ba1f323f2cdb2f246a861fb809c7539
ASIF231 <aa2312000@gmail.com>
2021-08-30 10:29:20 +0530

    First Release Version

--- app/src/main/java/com/asif23/diceroller/MainActivity.java
+++ app/src/main/java/com/asif23/diceroller/MainActivity.java
@@ -2,9 +2,11 @@ package com.asif23.diceroller;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -14,8 +16,9 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     Button btn;
-    String n;
+    int n;
     TextView tv;
+    ImageView imageViewDice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,16 +27,39 @@ public class MainActivity extends AppCompatActivity {
 
         btn = findViewById(R.id.buttonroll);
         tv= findViewById(R.id.textViewnumber);
+        imageViewDice = findViewById(R.id.dice);
+        final MediaPlayer mediaPlayersingledice = MediaPlayer.create(this,R.raw.move);
+
+
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                try {
-//                   Random j = new Random();
-                   List<Integer> givenList = Arrays.asList(1, 2, 3,4,5,6);
-                   Random rand = new Random();
-                   int j= givenList.get(rand.nextInt(givenList.size()));
-                   n = String.valueOf(j);
-                   tv.setText(n);
+                   mediaPlayersingledice.start();
+                   Random j = new Random();
+                   n = j.nextInt(6)+1;
+                   tv.setText(String.valueOf(n));
+
+                   switch (n) {
+                       case 1:
+                           imageViewDice.setImageResource(R.drawable.dice1);
+                           break;
+                       case 2:
+                           imageViewDice.setImageResource(R.drawable.dice2);
+                           break;
+                       case 3:
+                           imageViewDice.setImageResource(R.drawable.dice3);
+                           break;
+                       case 4:
+                           imageViewDice.setImageResource(R.drawable.dice4);
+                           break;
+                       case 5:
+                           imageViewDice.setImageResource(R.drawable.dice5);
+                           break;
+                       case 6:
+                           imageViewDice.setImageResource(R.drawable.dice6);
+                           break;
+                   }
                }
                catch (Exception e){
                    Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
